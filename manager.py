11478135115b74baf937d12672d750d4b9aab6d4
Feldherren <rpaliwoda@googlemail.com>
2021-03-19 00:42:22 +0000

    Sod it, all the changes.

--- manager.py
+++ manager.py
@@ -9,29 +9,19 @@ def read_config(config):
         config.read('manager.ini')
     else:
         make_config(config)
-        # config_object.read('manager.ini')
 
 def make_config(config):
     mod_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'mods')
     config['directories'] = {
-        'mod_folder': os.path.join(os.path.dirname(os.path.realpath(__file__)), 'mods'),
-        'angband_folder': None
         'mods': os.path.join(os.path.dirname(os.path.realpath(__file__)), 'mods'),
         'angband': None
     }
     with open('manager.ini', 'w') as configfile:
         config.write(configfile)
 
-def startup():
-    this_path = os.path.dirname(os.path.realpath(__file__))
 def startup(config):
     # confirm config file exists
-    # if not os.path.exists(os.path.join(this_path, 'mods'))
     read_config(config)
     # confirm mod folder exists
-
-read_config(config)
-
-print(config.sections())
     if not os.path.exists(config['directories']['mods']):
         os.mkdir(config['directories']['mods'])
